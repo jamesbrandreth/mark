95256055ff4da7ca9635eaf341b332f56ce2fc33
James Brandreth <james.brandreth@gmail.com>
2023-02-08 11:55:32 +0000

    Add injected style

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use glob::glob;
 use std::error::Error;
 use std::collections::HashMap;
 use tinytemplate::TinyTemplate;
@@ -9,12 +10,16 @@ use element::Element;
 
 pub fn main() -> Result<(), Box<dyn Error>> {
 
-    let templates: Vec<(String, String)> = fs::read_dir("./templates/default").unwrap().map(|entry| {
-        let path = entry.unwrap().path();
+    let templates: Vec<(String, String)> = glob::glob("./templates/default/*.html").unwrap().map(|entry| {
+        let path = entry.unwrap();
         let file_name = String::from(path.file_stem().unwrap().to_str().unwrap());
         let template = fs::read_to_string(&path).unwrap();
         (file_name, template)
     }).collect();
+
+    let styles: String = glob::glob("./templates/default/*.css").unwrap().map(|entry| {
+        fs::read_to_string(entry.unwrap().as_path()).unwrap()
+    }).collect::<String>();
 
     let mut tt = TinyTemplate::new();
     tt.set_default_formatter(&tinytemplate::format_unescaped);
@@ -29,6 +34,7 @@ pub fn main() -> Result<(), Box<dyn Error>> {
         template: "document".to_string(),
         content: HashMap::from([
             ("title".to_string(), "A Test Document".to_string()),
+            ("style".to_string(), styles),
         ]),
         parent: None,
         children: vec![
